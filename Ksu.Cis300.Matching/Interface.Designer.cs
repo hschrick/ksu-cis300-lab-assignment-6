1c9b4b2a1c457ca714f3640e39c95af828457e12
hschrick <hschrick@ksu.edu>
2017-01-30 11:56:16 -0600

    finished

---
+++ Ksu.Cis300.Matching/Interface.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Ksu.Cis300.Matching
+{
+    partial class ParenthesisMatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxTextBox = new System.Windows.Forms.TextBox();
+            this.uxCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter String:";
+            // 
+            // uxTextBox
+            // 
+            this.uxTextBox.Location = new System.Drawing.Point(91, 25);
+            this.uxTextBox.Name = "uxTextBox";
+            this.uxTextBox.Size = new System.Drawing.Size(181, 20);
+            this.uxTextBox.TabIndex = 1;
+            // 
+            // uxCheck
+            // 
+            this.uxCheck.Location = new System.Drawing.Point(91, 51);
+            this.uxCheck.Name = "uxCheck";
+            this.uxCheck.Size = new System.Drawing.Size(93, 36);
+            this.uxCheck.TabIndex = 2;
+            this.uxCheck.Text = "Check";
+            this.uxCheck.UseVisualStyleBackColor = true;
+            this.uxCheck.Click += new System.EventHandler(this.uxCheck_Click);
+            // 
+            // ParenthesisMatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 110);
+            this.Controls.Add(this.uxCheck);
+            this.Controls.Add(this.uxTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ParenthesisMatcher";
+            this.Text = "Parenthesis Matcher";
+            this.Load += new System.EventHandler(this.ParenthesisMatcher_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox uxTextBox;
+        private System.Windows.Forms.Button uxCheck;
+    }
+}
+
---
+++ Ksu.Cis300.Matching/Interface.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+/*
+ * Author <Harris Schrick>
+ * 
+ */
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ksu.Cis300.Matching
+{
+    public partial class ParenthesisMatcher : Form
+    {
+        public ParenthesisMatcher()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// checks if parenthesis match
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxCheck_Click(object sender, EventArgs e)
+        {
+            string test = uxTextBox.Text;
+            Stack<char> s = new Stack<char>();
+
+            foreach (char c in test)
+            {
+                //opening bracket
+                if (c == '(' ||c == '{'||c == '[')
+                {
+                    s.Push(c);
+                }
+                //closing bracket
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (s.Count > 0 && Matches(s.Peek(),c))
+                    {
+                        s.Pop();
+                    }
+                    else
+                    {
+                        ShowError();
+                        return;
+
+                    }
+                }
+
+
+
+
+            }
+            if(s.Count == 0)
+            {
+                ShowSuccess();
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+
+
+        /// <summary>
+        /// Determines whether the given character is an opening parenthesis.
+        /// </summary>
+        /// <param name="c">The character to check.</param>
+        /// <returns>Whether c is an opening parenthesis.</returns>
+        private bool IsOpeningParenthesis(char c)
+        {
+            return c == '(' || c == '[' || c == '{';
+        }
+
+        /// <summary>
+        /// Determines whether the given character is a closing parenthesis.
+        /// </summary>
+        /// <param name="c">The character to check.</param>
+        /// <returns>Whether c is a closing parenthesis.</returns>
+        private bool IsClosingParenthesis(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
+
+        /// <summary>
+        /// Determines whether the given characters for a matched pair
+        /// of parentheses.
+        /// </summary>
+        /// <param name="a">The opening character.</param>
+        /// <param name="b">The closing character.</param>
+        /// <returns>Whether a and b form a matched pair of parentheses.</returns>
+        private bool Matches(char a, char b)
+        {
+            return (a == '(' && b == ')') || (a == '[' && b == ']') ||
+                (a == '{' && b == '}');
+        }
+
+        /// <summary>
+        /// Displays a success message.
+        /// </summary>
+        private void ShowSuccess()
+        {
+            MessageBox.Show("The string is matched.");
+        }
+
+        /// <summary>
+        /// Displays an error message.
+        /// </summary>
+        private void ShowError()
+        {
+            MessageBox.Show("The string is not matched.");
+        }
+
+        private void ParenthesisMatcher_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
